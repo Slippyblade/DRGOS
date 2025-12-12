755e4c37f85792d637c22fdea0ab7e10a50597b0
Slippyblade <Slippyblade@gmail.com>
2025-12-11 19:56:07 -0700

    End of day 12/11

--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from catalog.models import CatalogItem
 
-# Create your views here.
+
+def shop(request):
+    products = CatalogItem.objects.filter(online=True)
+    return render(request, 'ecom/shop.html', {'pageTitle': 'Shop in style', 'pageSubtitle': 'With this shop homepage template', 'products': products})
+
+def about(request):
+    return render(request, 'ecom/about.html', {'pageTitle': 'About Us', 'pageSubtitle': 'Learn more about our company'})
