5ba4a5dc4742d008d30306fdc78e890a92bfa987
Stephen <slippyblade@gmail.com>
2025-12-14 00:02:06 -0700

    End of day 12/13

--- ecom/views.py
+++ ecom/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from catalog.models import CatalogItem
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -12,7 +12,21 @@ def about(request):
     return render(request, 'ecom/about.html', {'pageTitle': 'About Us', 'pageSubtitle': 'Learn more about our company'})
 
 def login_user(request):
-    return render(request, 'ecom/login.html', {'pageTitle': 'Login', 'pageSubtitle': 'Login to your account.'})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password'] 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Login Successful!"))
+            return redirect('shop')
+        else:
+            messages.success(request, ("Login failed, please try again..."))
+            return redirect('login')
+    else:        
+        return render(request, 'ecom/login.html', {'pageTitle': 'Login', 'pageSubtitle': 'Login to your account.'})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, ("You were successfully logged out."))
+    return redirect('shop')
